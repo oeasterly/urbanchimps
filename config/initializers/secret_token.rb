1d735f0129cd1ec59a83456f99845eb4c075f6d1
oeasterly <oamoravet@yahoo.com>
2014-08-20 12:41:25 +1200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Urbanchimps::Application.config.secret_key_base = '6a659b335da91c44526f64801feb490dffc74b1e28f455d9c1a1c65d09c3311e0d4af757af6db8b4cc46666b1611130a8a3f4f9904d79f5ab84b919ac4efa06d'
